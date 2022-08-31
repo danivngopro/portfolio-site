dfa246484b7597468268165c47d8b964da4881d0
danivngopro <danivngopro@gmail.com>
2022-08-31 14:41:50 +0300

    adding the option to download my cv as a pdf and an extremly cool looking button

--- portfolio-site-ui/src/pages/cvPage/CVPage.js
+++ portfolio-site-ui/src/pages/cvPage/CVPage.js
@@ -2,7 +2,16 @@ import "./CVPage.css";
 
 function CVPage() {
   return (
-    <h1>cv page</h1>
+    <>
+      <h1>cv page</h1>
+      <h1>here we go this is my cv.</h1>
+      <h1>fk you now go on with your life and imagine its a real cv ok???</h1>
+      <a
+      href="Daniel Ventura CV.pdf"
+      download="Daniel Ventura CV.pdf">
+        <button>Resume download</button>
+      </a>
+    </>
   );
 }
 
--- portfolio-site-ui/src/pages/homePage/HomePage.js
+++ portfolio-site-ui/src/pages/homePage/HomePage.js
@@ -1,8 +1,18 @@
 import "./HomePage.css";
-
+import Button from '@mui/material/Button';
 function HomePage() {
   return (
-    <h1>home page</h1>
+    <div className="home-page-container">
+      <div className="home-page-name">
+        <p className="home-page-name_big">Daniel Ventura</p>
+        <p className="home-page-name_medium">Full Stack developer</p>
+      </div>
+      <div className="home-page-transferer">
+        <p className="home-page-transferer_text">if you're here, you have come to check me out</p>
+        <p className="home-page-transferer_text">well then?</p>
+        <button className="home-page-transferer_button"> let's get started</button>
+      </div>
+    </div>
   );
 }
 
